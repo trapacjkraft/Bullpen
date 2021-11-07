3343d5c1eb95e6d6b3da20b1ffd0c554c90402af
Joshua Kraft <jkraft@trapac.com>
2021-11-06 21:43:42 -0500

    Initial Commit

---
+++ Bullpen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bullpen
+//
+//  Created by Joshua Kraft on 11/6/21.
+//  Copyright © 2021 Joshua Kraft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Bullpen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bullpen
+//
+//  Created by Joshua Kraft on 11/6/21.
+//  Copyright © 2021 Joshua Kraft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
